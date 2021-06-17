dc0392ebd4f2d98b1ff09589d93a6248b75b5c75
“Christine <csalonen89@gmail.com>
2021-06-16 20:21:51 -0700

    add hide styling for result of triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
 $("#triangle-form").submit(function(event) {
   event.preventDefault();
-  const side1 = parseInt($("#side1").val());
-  const side2 = parsetInt($("#side2").val());
-  const side3 = parseInt($("#side3").val());
-  
+  const s1 = parseInt($("#side1").val());
+  const s2 = parseInt($("#side2").val());
+  const s3 = parseInt($("#side3").val());
+
+  if (s1 + s2 <= s3 || s1 + s3 <= s2 || s2 + s3 < s1 ) {
+    triangle = "NOT A TRIANGLE"; 
+  } else if ( s1 != s2 && s2 != s3) {
+    triangle = "Scalene";
+    console.log(s2);
+    console.log(triangle);
+  } else if ( s1 === s2 && s2 != s3 || s2 === s3 && s2 != s1 ) {
+    triangle = "Isosceles";
+  } else if (s1 === s2 && s1 === s3 && s2 === s3) {
+    triangle = "Equilateral";
+  } else {
+    triangle = "try again";
+    console.log(triangle);
+  }
+
+  $("#triangle-type").show();
+  $("#triangle").text(triangle);
   });
 });
