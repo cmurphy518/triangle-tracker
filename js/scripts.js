04caffb2c5769bfc47b5563fd1d722ebd788eb5a
“Christine <csalonen89@gmail.com>
2021-06-16 19:54:31 -0700

    add index html to create webpage for triangle tracker. We will request users to submit three numbers to create a type of triangle

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+$("#triangle-form").submit(function(event) {
+  event.preventDefault();
+  const side1 = parseInt($("#side1").val());
+  const side2 = parsetInt($("#side2").val());
+  const side3 = parseInt($("#side3").val());
+  
+  });
+});
